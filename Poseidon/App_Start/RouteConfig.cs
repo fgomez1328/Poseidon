2507e38b47794dd5f4318d515dc896cb688b5073
jherrera2014 <jherrera@dnkwater.com>
2015-02-06 16:06:10 -0400

    home controller estable

--- Poseidon/App_Start/RouteConfig.cs
+++ Poseidon/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Poseidon
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "LogOut", id = UrlParameter.Optional }
             );
         }
     }
